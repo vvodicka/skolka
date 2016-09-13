a38909e42aad424687b14c7fa0d50d826d8c2acc
Vlado <vladislav.vodicka@gmail.com>
2016-09-13 19:26:52 +0200

    google maps api key

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,22 +8,22 @@
  * Controller of the skolkaApp
  */
 angular.module('skolkaApp')
-  .controller('ContactCtrl', function ($scope, $location, $anchorScroll) {
-      $scope.map = {
-          center: { latitude: 48.102315, longitude: 17.232117 },
-          zoom: 10,
-            options: {mapTypeControl : false}
-      };
+    .controller('ContactCtrl', function ($scope, $location, $anchorScroll) {
+        $scope.map = {
+            center: {latitude: 48.102315, longitude: 17.232117},
+            zoom: 10,
+            options: {mapTypeControl: false},
+        };
 
-      $scope.zoomToRuzinov = function() {
-          $scope.map = { center: { latitude: 48.140011, longitude: 17.171488 }, zoom: 17 };
-          $location.hash('map');
-          $anchorScroll();
-      };
+        $scope.zoomToRuzinov = function () {
+            $scope.map = {center: {latitude: 48.140011, longitude: 17.171488}, zoom: 17};
+            $location.hash('map');
+            $anchorScroll();
+        };
 
-      $scope.zoomToDunajskaLuzna = function() {
-          $scope.map = { center: { latitude: 48.074811, longitude: 17.275818 }, zoom: 17 };
-          $location.hash('map');
-          $anchorScroll();
-      }
-  });
+        $scope.zoomToDunajskaLuzna = function () {
+            $scope.map = {center: {latitude: 48.074811, longitude: 17.275818}, zoom: 17};
+            $location.hash('map');
+            $anchorScroll();
+        }
+    });
