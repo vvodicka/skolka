a8be49a2222caed44503a88f67ce1ab194f5c192
Vlado <vladislav.vodicka@gmail.com>
2016-09-14 22:04:16 +0200

    opravy

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,22 +8,29 @@
  * Controller of the skolkaApp
  */
 angular.module('skolkaApp')
-    .controller('ContactCtrl', function ($scope, $location, $anchorScroll) {
-        $scope.map = {
-            center: {latitude: 48.102315, longitude: 17.232117},
-            zoom: 10,
-            options: {mapTypeControl: false},
-        };
+    .controller('ContactCtrl', function ($scope, $location, $anchorScroll, uiGmapGoogleMapApi, $timeout) {
+
+        uiGmapGoogleMapApi.then(function (maps) {
+            $timeout(function () {
+                $scope.map = {
+                    center: {latitude: 48.102315, longitude: 17.232117},
+                    zoom: 10,
+                    options: {mapTypeControl: false}
+                };
+
+                $scope.showMap = true;
+            }, 100);
+        });
 
         $scope.zoomToRuzinov = function () {
             $scope.map = {center: {latitude: 48.140011, longitude: 17.171488}, zoom: 17};
-            $location.hash('map');
-            $anchorScroll();
+            //$location.hash('map');
+            $anchorScroll('map');
         };
 
         $scope.zoomToDunajskaLuzna = function () {
             $scope.map = {center: {latitude: 48.074811, longitude: 17.275818}, zoom: 17};
-            $location.hash('map');
-            $anchorScroll();
+            //$location.hash('map');
+            $anchorScroll('map');
         }
     });
